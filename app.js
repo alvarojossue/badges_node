00287c684898904e6de06dfbbb861cd984dfbbf7
Alvaro Castillo <alvaro.castillo23@gmail.com>
2016-12-27 17:36:37 -0500

    Parses and prints data

--- app.js
+++ app.js
@@ -3,21 +3,30 @@
 
 var https = require("https")
 
+var username = "alvarocastillo"
+
 function printMessage(username, badgeCount, points){
 	var message = username + " has " + badgeCount + " total badge(s) and " + points + " points in JavaScript";
 	console.log(message);
 }
 //Connect to API URL (http://teamtreehouse.com/username.json)
-	var request = https.get("https://teamtreehouse.com/alvarocastillo.json", function(response){
+	var request = https.get("https://teamtreehouse.com/" + username + ".json", function(response){
 		var body = ""
+		
 		//Read the data
+
 		response.on('data', function(chunk){
 			body += chunk;
 		})
 		response.on("end", function(){
-			console.log(body);
+			var profile = JSON.parse(body)
+			printMessage(username, profile.badges.length, profile.points.JavaScript)
 		})
+
 		//Parse the data
+
+
+
 		//Print the data
 	})
 
